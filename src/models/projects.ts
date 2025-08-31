b1b1ac6792431f7be7ae6f1ae730a182432aecbd
Lucas Navarro <lucas.navarro@lilab.pe>
2025-08-31 16:36:27 -0300

    feat: test changes

--- src/models/projects.ts
+++ src/models/projects.ts
@@ -1,73 +1,65 @@
 export interface Project {
-  position: string;
-  company: string;
-  date: {
-    start: Date;
-    end: Date | "Present";
-  };
-  roleDescription: string;
-  achievements: string[];
+    title: string;
+    description: string;
+    link: string;
+    image: string | null;
+    tags: string[];
 }
-export const EXPERIENCE_TIMELINE: Project[] = [
+export const PROJECTS: Project[] = [
     {
-        position: "Full-stack Developer & IA Creative Technologist",
-        company: "Lima Innovation Lab.",
-        date: {
-            start: new Date("2024-04-01"),
-            end: "Present",
-        },
-        roleDescription:
-            "Lideré el desarrollo de un producto SaaS con integración de IA, optimizando flujos de trabajo y coordinación entre equipos, desplegando soluciones full-stack escalables en Azure, AWS y GCP.",
-        achievements: [
-            "Rediseño completo del dashboard.",
-            "Optimización de carga en un 30%.",
-            "Mentoría a desarrolladores junior.",
-        ],
+        title: "Autonomous Multi-Agent System",
+        description:
+            "A demo of our multi-agent phone assistant handling restaurant reservations in real time. See how the system manages bookings seamlessly and efficiently!",
+        link: "",
+        image: null,
+        tags: ["React", "Redux", "Firebase", "Material-UI"],
+    },
+     {
+        title: "Carpenter Composer: Command line Interface",
+        description:
+            "Figma-to-code automation CLI bridging the gap between design and development, improving product quality by streamlining collaboration across teams that speak different 'languages'.",
+        link: "",
+        image: null,
+        tags: ["React", "Redux", "Firebase", "Material-UI"],
     },
     {
-        position: "Full-stack Developer",
-        company: "Lima Innovation Lab.",
-        date: {
-            start: new Date("2023-07-01"),
-            end: new Date("2024-04-01"),
-        },
-        roleDescription:
-            "Desarrollé integralmente un producto SaaS con automatización basada en agentes de IA, participando en diseño, desarrollo, pruebas y despliegue, garantizando escalabilidad y eficiencia en la entrega de proyectos.",
-        achievements: [
-            "Lanzamiento del sistema de suscripciones.",
-            "Desarrollo de APIs REST para app móvil.",
-            "Automatización de tests reduciendo bugs en 40%.",
-            "Configuración de pipelines CI/CD.",
-        ],
+        title: "Viuti 2.0",
+        description:
+            "Next-generation business management CMS, expanding capabilities and simplifying functionality for more powerful client use, including a training platform for seamless adoption.",
+        link: "",
+        image: "/images/projects/prickly.jpg",
+        tags: ["React", "Redux", "Firebase", "Material-UI"],
     },
     {
-        position: "Front-end Developer",
-        company: "Lima Innovation Lab.",
-        date: {
-            start: new Date("2023-05-01"),
-            end: new Date("2023-06-30"),
-        },
-        roleDescription:
-            "Mantenimiento y desarrollo front-end en un producto SaaS, optimizando interfaces y flujos de trabajo, coordinando con el equipo de diseño para mejorar la eficiencia y la experiencia de usuario.",
-        achievements: [
-            "Mantenimiento de código legacy.",
-            "Soporte a clientes internos.",
-        ],
+        title: "Viutify",
+        description:
+            "Business management CMS integrating resources, finance, and operations to optimize and streamline daily processes.",
+        link: "",
+        image: "/images/projects/viuti-2.0.jpg",
+        tags: ["React", "Redux", "Firebase", "Material-UI"],
     },
     {
-        position: "Full-stack Developer",
-        company: "Fiverr (freelance)",
-        date: {
-            start: new Date("2021-06-01"),
-            end: new Date("2023-03-01"),
-        },
-        roleDescription:
-            "Desarrollé soluciones full-stack personalizadas para clientes, incluyendo integración de IA, automatización de flujos y despliegue en múltiples plataformas, garantizando rendimiento y escalabilidad.",
-        achievements: [
-            "Mantenimiento de código legacy.",
-            "Soporte a clientes internos.",
-            "Optimización de procesos internos de clientes.",
-            "Implementación de sistemas modulares y escalables.",
-        ],
+        title: "Chapter lead challange",
+        description:
+            "Task management platform with project organization, featuring an authentication system. The app boasts an intuitive interface and provides administrative tools for renaming and deleting projects",
+        link: "",
+        image: null,
+        tags: ["React", "Redux", "Firebase", "Material-UI"],
+    },
+    {
+        title: "Socio sostenible",
+        description:
+            "SocioSostenible helps organizations adopt sustainable practices aligned with UN SDGs by using AI to provide tailored solutions, easing the path to recognized licenses and certifications.",
+        link: "",
+        image: "/images/projects/socio-sostenible.jpg",
+        tags: ["React", "Redux", "Firebase", "Material-UI"],
+    },
+    {
+        title: "Runo CLI",
+        description:
+            "Figma-to-code automation CLI bridging the gap between design and development, improving product quality by streamlining collaboration across teams that speak different 'languages'.",
+        link: "",
+        image: null,
+        tags: ["React", "Redux", "Firebase", "Material-UI"],
     },
 ];
